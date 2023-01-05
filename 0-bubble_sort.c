f0a19b2ae80a7a3f9fcb754b09bd1d93f0a7c7dc
dariomnalerio <dariomnalerio@gmail.com>
2023-01-05 05:56:03 -0800

    Task 0 betty fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,27 @@
 #include "sort.h"
 
 /**
- *
- *
- *
- *
+ * bubble_sort -  sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ * @array: array to be ordered
+ * @size: size of the array
 */
 
 void bubble_sort(int *array, size_t size)
 {
 	int temp = 0;
+	size_t i, j, k;
 
-	for (size_t i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (size_t j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				for (size_t k = 0; k < size; k++)
+				for (k = 0; k < size; k++)
 				{
 					printf("%d ", array[k]);
 				}
