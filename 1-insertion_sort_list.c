8b3cf90127947ce93b51a6a831edab2ee42b055f
gabrielblasina <5722@holbertonshool.com>
2023-01-10 13:52:30 -0800

    task-1 of insertion sort proyect

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+void _swap(listint_t *current, listint_t *previous);
+/**
+ * insertion_sort_list - function to sort a doubly linked list
+ * Return: Void
+ * @list: pointer to the linked list
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *previous;
+
+	if ((*list != NULL) && ((*list)->next != NULL))
+	{
+		current = (*list)->next;
+		while (current)
+		{
+			previous = current->prev;
+			while ((previous) && ((current->n) < (previous->n)))
+			{
+				_swap(current, previous);
+				previous = current->prev;
+				if (current->prev == NULL)
+					*list = current;
+				print_list(*list);
+			}
+			current = current->next;
+		}
+	}
+}
+/**
+ * _swap - function to swap two nodes
+ * Return: Void
+ * @current: The node with the biggest value
+ * @previous: The node with the smallest value
+*/
+void _swap(listint_t *current, listint_t *previous)
+{	
+	if (current->next)
+		current->next->prev = previous;
+	previous->next = current->next;
+	if (previous->prev)
+		previous->prev->next = current;
+	current->next = previous;
+	current->prev = previous->prev;
+	previous->prev = current;
+}
--- sort.h
+++ sort.h
@@ -1,8 +1,7 @@
-#ifndef sort_h
-#define sort_h
-
+#ifndef SORT_H
+#define SORT_H
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -12,16 +11,13 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
-
-/* Function prototypes */
-
-void print_array(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
-
+listint_t *add_nodeint_at_index(listint_t **head, int n, int index);
+listint_t *get_nodeint_at_index(listint_t *head, int index);
+int remove_nodeint_at_index(listint_t **head, int index);
 #endif
