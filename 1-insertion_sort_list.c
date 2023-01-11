39dda01c8ae4b7370b2b8701710f7a8ea3fce47d
gabrielblasina <5722@holbertonshool.com>
2023-01-10 18:46:06 -0800

    task-1 NULL case fixed

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *previous;
 
-	if ((*list != NULL) && ((*list)->next != NULL))
+	if ((list != NULL) && (*list != NULL) && ((*list)->next != NULL))
 	{
 		current = (*list)->next;
 		while (current)
